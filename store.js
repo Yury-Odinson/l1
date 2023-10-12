d07e4b1a2cd4d8c24a82c4f48c0029443bebf071
Yury <yury.odinson@gmail.com>
2023-10-12 23:30:10 +0300

    add: task6 and store

---
+++ store.js
@@ -0,0 +1,41 @@
+// здесь будут хранится большие объекты/массивы и т.д. для заданий
+
+
+// task 6:
+export const users = [{
+    name: "Bonny",
+    age: 71
+}, {
+    name: "Zikk",
+    age: 43
+}, {
+    name: "Joe",
+    age: 67
+}, {
+    name: "Elisabeth",
+    age: 25
+}, {
+    name: "Nikky",
+    age: 27
+}, {
+    name: "Zakk",
+    age: 43
+}, {
+    name: "Alfred",
+    age: 25
+}, {
+    name: "Jenny",
+    age: -43
+}, {
+    name: "Трус",
+    age: 84
+}, {
+    name: "Бывалый",
+    age: 75
+}, {
+    name: "Балбес",
+    age: 75
+}, {
+    name: "Ёжин с бажин",
+    age: 56
+}]
