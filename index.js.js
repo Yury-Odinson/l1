d4f88d36432b0c08c294f1224e7445e13abe25df
yury-odinson <yury.odinson@gmail.com>
2023-10-16 16:49:41 +0300

    add: task 16

---
+++ index.js.js
@@ -0,0 +1,5 @@
+import { getDate } from "./task_16.js";
+
+// пример работы 16-ого задания на работу с модулями и внешними библиотеками:
+console.log(getDate.thisDate())     // вывод сегодняшнего числа
+console.log(getDate.thisWeek())     // вывод номера недели в году
