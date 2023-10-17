de33be41e05879ccf94dffda98bf41b6522e52d1
Yury <yury.odinson@gmail.com>
2023-10-18 00:14:57 +0300

    add: task 24 without sort

--- task_24.js
+++ task_24.js
@@ -11,17 +11,33 @@ import { createElement } from "./task_25.js"
 const dataURL = "http://www.filltext.com/?rows=1000&fname=%7BfirstName%7D&lname=%7BlastName%7D&tel=%7Bphone%7Cformat%7D&address=%7BstreetAddress%7D&city=%7Bcity%7D&state=%7BusState%7Cabbr%7D&zip=%7Bzip%7D&pretty=true"
 const bloclData = document.getElementById("users-data")
 const bloclPagination = document.getElementById("pagination")
-const data = []     // декларация конечного массива результатов, разделённого на части (для пагинации)
+
+const sortFNameA = document.getElementById("sortFnameA")
+const sortFNameD = document.getElementById("sortFnameD")
+const sortLNameA = document.getElementById("sortLnameA")
+const sortLNameD = document.getElementById("sortLnameD")
+const sortPhoneA = document.getElementById("sortPhoneA")
+const sortPhoneD = document.getElementById("sortPhoneD")
+const sortAddressA = document.getElementById("sortAddressA")
+const sortAddressD = document.getElementById("sortAddressD")
+const sortCityA = document.getElementById("sortCityA")
+const sortCityD = document.getElementById("sortCityD")
+const sortStateA = document.getElementById("sortStateA")
+const sortStateD = document.getElementById("sortStateD")
+const sortZipA = document.getElementById("sortZipA")
+const sortZipD = document.getElementById("sortZipD")
+
+const test = [sortFNameA, sortFNameD, sortLNameA, sortLNameD, sortPhoneA, sortPhoneD, sortAddressA, sortAddressD, sortCityA, sortCityD, sortStateA, sortStateD, sortZipA, sortZipD]
+
+let resultData = []     // декларация массива, для общего списка
+let resultDataPage = [] // декларация массива, для подмассива (для пагинации)
+// let 
 
 async function getData(url) {
     const response = await fetch(url).then(response => {
         return response.json()
     })
-    for (let i = 0; i < response.length; i += 50) {
-        data.push(response.slice(i, i + 50))
-    }
-    pagination(data)
-    drawData(data[0])
+    return resultData = response
 }
 
 function drawData(arr) {
@@ -31,9 +47,9 @@ function drawData(arr) {
         const firstName = createElement("td")
         firstName.textContent = element.fname
         const lastName = createElement("td")
-        lastName.textContent = element.lastName
+        lastName.textContent = element.lname
         const phone = createElement("td")
-        phone.textContent = element.phone
+        phone.textContent = element.tel
         const address = createElement("td")
         address.textContent = element.address
         const city = createElement("td")
@@ -43,19 +59,81 @@ function drawData(arr) {
         const zip = createElement("td")
         zip.textContent = element.zip
         item.append(firstName, lastName, phone, address, city, state, zip)
-        bloclData.appendChild(item)
+        bloclData.append(item)
     })
 }
 
 function pagination(arr) {
-    for (let i = 0; i < arr.length; i++) {
+    const dataPages = []
+    let thisPage = []
+    for (let i = 0; i < arr.length; i += 50) {     // разбиение результата на подмассивы для пагинации
+        dataPages.push(arr.slice(i, i + 50))
+    }
+    for (let i = 0; i < dataPages.length; i++) {
         const button = createElement("button", ["pagination__item"])
         button.textContent = i + 1
         bloclPagination.append(button)
         button.addEventListener("click", () => {
-            drawData(arr[button.textContent - 1])
+            drawData(dataPages[button.textContent - 1])
         })
+        thisPage = dataPages[i]
     }
+    return resultDataPage = thisPage
 }
 
-// getData(dataURL)
+
+function sort(arr) {
+    let result = []
+
+    let sortMethod = ""
+
+    test.map(element => {
+        element.addEventListener("click", () => {
+            // console.log(element.id)
+            sortMethod = element.id;
+            console.log(sortMethod)
+            // sort(arr, sortMethod)
+        })
+    })
+    console.log(sortMethod)
+    switch (sortMethod) {
+        // case "sortFnameA": result = arr.sort((a, b) => a.fname > b.fname ? 1 : -1);
+        case "sortFnameA": console.log("sg");
+            break;
+        case "sortFnameD": result = arr.sort((a, b) => a.fname < b.fname ? 1 : -1);
+            break;
+        case "sortLnameA": result = arr.sort((a, b) => a.lname > b.lname ? 1 : -1);
+            break;
+        case "sortLnameD": result = arr.sort((a, b) => a.lname < b.lname ? 1 : -1);
+            break;
+        case "sortPhoneA": result = arr.sort((a, b) => a.tel > b.tel ? 1 : -1);
+            break;
+        case "sortPhoneD": result = arr.sort((a, b) => a.tel < b.tel ? 1 : -1);
+            break;
+        case "sortAddressA": result = arr.sort((a, b) => a.address > b.address ? 1 : -1);
+            break;
+        case "sortAddressD": result = arr.sort((a, b) => a.address < b.address ? 1 : -1);
+            break;
+        case "sortCityA": result = arr.sort((a, b) => a.city > b.city ? 1 : -1);
+            break;
+        case "sortCityD": result = arr.sort((a, b) => a.city < b.city ? 1 : -1);
+            break;
+        case "sortStateA": result = arr.sort((a, b) => a.state > b.state ? 1 : -1);
+            break;
+        case "sortStateD": result = arr.sort((a, b) => a.state < b.state ? 1 : -1);
+            break;
+        case "sortZipA": result = arr.sort((a, b) => a.zip > b.zip ? 1 : -1);
+            break;
+        case "sortZipD": result = arr.sort((a, b) => a.zip < b.zip ? 1 : -1);
+            break;
+        default: return arr     // при других вариантах сортировка не происходит
+        // default: return console.log("asdfg")     // при других вариантах сортировка не происходит
+    }
+    // console.log(result)
+    return drawData(result)
+}
+getData(dataURL).then(() => {
+    pagination(resultData)
+    drawData(resultDataPage)    // отрисовка первых значений по умолчанию, после загрузки всех данных
+    sort(resultDataPage)
+})
