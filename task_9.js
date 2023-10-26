1080494944ee33e84f3a86e9fcb79d77acdce3fe
yury-odinson <yury.odinson@gmail.com>
2023-10-26 13:09:42 +0300

    add: task 9 eval

--- task_9.js
+++ task_9.js
@@ -1,6 +1,6 @@
 // Реализовать функцию конвертации JSON в строку
 
-const jsonExample = '[{"name":"Valeria","password":"qwerty","age":27,"children":false},{"name":"Yury","login":"alice","age":28,"children":false}]'
+const jsonExample = '[{"name":"Valeria","login":"qwerty","age":27,"children":false},{"name":"Yury","login":"alice","age":28,"children":false}]'
 const result = [
     {
         name: 'Valeria',
@@ -31,20 +31,18 @@ function getString(jsonString) {
     // (?<=,)"  - убрать кавычки после знака ,
     // "(?=:)   - убрать кавычки перед знаком :
     // "(?=\})  - убрать кавычки перед знаком }
-    const result = jsonString.replace(/(?<=[{}])"|(?<=,)"|"(?=:)|"(?=\})/g, "")
+    // преобразование в строку без лишних символов
+    const convert = jsonString.replace(/(?<=[{}])"|(?<=,)"|"(?=:)|"(?=\})/g, "")
+    // преобразование строки в объект
+    
+    const result = eval("(" + convert + ")")
 
-    console.log(jsonString)
+    const parse = JSON.parse(jsonString)
+
+    console.log(parse)
     console.log(result)
 
     return result
 }
 
 getString(jsonExample)
-
-// const test = {
-//     name:"Иван",
-//     age:37,
-//     mother:{name:"Ольга",age:58},
-//     children:["Маша",Игорь",Таня"],
-//     married: true,
-//     dog:null}
